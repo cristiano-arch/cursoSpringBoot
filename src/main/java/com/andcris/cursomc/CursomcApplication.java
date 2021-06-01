87da0a1d9d678b1664dfe9599c3fa1ced54fc8c1
Cristiano Jr <cristianoandre96@gmail.com>
2021-06-01 20:20:28 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/andcris/cursomc/CursomcApplication.java
+++ src/main/java/com/andcris/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.andcris.cursomc.domain.Cidade;
 import com.andcris.cursomc.domain.Cliente;
 import com.andcris.cursomc.domain.Endereco;
 import com.andcris.cursomc.domain.Estado;
+import com.andcris.cursomc.domain.ItemPedido;
 import com.andcris.cursomc.domain.Pagamento;
 import com.andcris.cursomc.domain.PagamentoComBoleto;
 import com.andcris.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.andcris.cursomc.repositories.CidadeRepository;
 import com.andcris.cursomc.repositories.ClienteRepository;
 import com.andcris.cursomc.repositories.EnderecoRepository;
 import com.andcris.cursomc.repositories.EstadoRepository;
+import com.andcris.cursomc.repositories.ItemPedidoRepository;
 import com.andcris.cursomc.repositories.PagamentoRepository;
 import com.andcris.cursomc.repositories.PedidoRepository;
 import com.andcris.cursomc.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);		
@@ -123,6 +128,23 @@ public class CursomcApplication implements CommandLineRunner{
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
+		
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 
 }
--- src/main/java/com/andcris/cursomc/domain/Pedido.java
+++ src/main/java/com/andcris/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.andcris.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -31,6 +34,9 @@ public class Pedido implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
+	
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	public Pedido() {
 		
@@ -83,6 +89,14 @@ public class Pedido implements Serializable{
 
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
+	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
