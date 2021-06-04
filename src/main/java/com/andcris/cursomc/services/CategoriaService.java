0b39d944c07143c6a9ca9c49518df0ec0a3f8423
Cristiano Jr <cristianoandre96@gmail.com>
2021-06-03 21:31:20 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/andcris/cursomc/services/CategoriaService.java
+++ src/main/java/com/andcris/cursomc/services/CategoriaService.java
@@ -15,7 +15,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado! Id: " + id + ", Tipo: "+ Categoria.class.getName()));
@@ -24,6 +24,11 @@ public class CategoriaService {
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);		
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
 	}
 	
 }
