73c45a1be83447f0d1bf3375502c1affeb33ec36
Cristiano Jr <cristianoandre96@gmail.com>
2021-06-15 22:26:12 -0300

    Personalizando o retorno da validacao

---
+++ src/main/java/com/andcris/cursomc/resources/exceptions/ValidationError.java
@@ -0,0 +1,23 @@
+package com.andcris.cursomc.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, Long timestamps) {
+		super(status, msg, timestamps);
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+}
