df1fa65a5570bfece6ae9de8cc6120cea071fa7c
Cristiano Jr <cristianoandre96@gmail.com>
2021-06-15 21:36:45 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/andcris/cursomc/services/CategoriaService.java
+++ src/main/java/com/andcris/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.andcris.cursomc.domain.Categoria;
+import com.andcris.cursomc.dto.CategoriaDTO;
 import com.andcris.cursomc.repositories.CategoriaRepository;
 import com.andcris.cursomc.services.exceptions.DataIntegrityException;
 import com.andcris.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,6 +56,12 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		
 		return repo.findAll(pageRequest);
+		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 		
 	}
 }
