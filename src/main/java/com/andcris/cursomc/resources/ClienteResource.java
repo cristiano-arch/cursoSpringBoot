49038ceee40f3009af34284967b2feec9cc87ac9
Cristiano Jr <cristianoandre96@gmail.com>
2021-06-17 21:26:18 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/andcris/cursomc/resources/ClienteResource.java
+++ src/main/java/com/andcris/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.andcris.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andcris.cursomc.domain.Cliente;
 import com.andcris.cursomc.dto.ClienteDTO;
+import com.andcris.cursomc.dto.ClienteNewDTO;
 import com.andcris.cursomc.services.ClienteService;
 
 @RestController
@@ -32,6 +35,19 @@ public class ClienteResource {
 		Cliente obj = service.find(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)	
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO){
+	
+		Cliente obj = service.fromDTO(objDTO);
+		
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
--- src/main/java/com/andcris/cursomc/services/ClienteService.java
+++ src/main/java/com/andcris/cursomc/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.andcris.cursomc.domain.Cidade;
 import com.andcris.cursomc.domain.Cliente;
+import com.andcris.cursomc.domain.Endereco;
+import com.andcris.cursomc.domain.enums.TipoCliente;
 import com.andcris.cursomc.dto.ClienteDTO;
+import com.andcris.cursomc.dto.ClienteNewDTO;
 import com.andcris.cursomc.repositories.ClienteRepository;
+import com.andcris.cursomc.repositories.EnderecoRepository;
 import com.andcris.cursomc.services.exceptions.DataIntegrityException;
 import com.andcris.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -21,6 +27,9 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -28,6 +37,14 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto nao encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 
+	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 
 	public Cliente update(Cliente obj) {
@@ -61,6 +78,22 @@ public class ClienteService {
 
 		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
 
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()));
+		Cidade cid = new Cidade(objDTO.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		if(objDTO.getTelefone2()!=null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		if(objDTO.getTelefone3()!=null) {
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		
+		return cli;
 	}
 	
 	private void updateData(Cliente newObj, Cliente obj) {
