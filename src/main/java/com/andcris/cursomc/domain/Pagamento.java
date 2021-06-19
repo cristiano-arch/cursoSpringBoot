0acea5d4b7fe9c2903e04bf36e44bc5044dcf3af
Cristiano Jr <cristianoandre96@gmail.com>
2021-06-19 16:13:57 -0300

    Inserindo Pedido

--- src/main/java/com/andcris/cursomc/domain/Pagamento.java
+++ src/main/java/com/andcris/cursomc/domain/Pagamento.java
@@ -12,9 +12,11 @@ import javax.persistence.OneToOne;
 
 import com.andcris.cursomc.domain.enums.EstadoPagamento;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 @Inheritance(strategy = InheritanceType.JOINED)
 @Entity
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@type")
 public abstract class Pagamento implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
--- src/main/java/com/andcris/cursomc/services/PedidoService.java
+++ src/main/java/com/andcris/cursomc/services/PedidoService.java
@@ -1,11 +1,18 @@
 package com.andcris.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.andcris.cursomc.domain.ItemPedido;
+import com.andcris.cursomc.domain.PagamentoComBoleto;
 import com.andcris.cursomc.domain.Pedido;
+import com.andcris.cursomc.domain.enums.EstadoPagamento;
+import com.andcris.cursomc.repositories.ItemPedidoRepository;
+import com.andcris.cursomc.repositories.PagamentoRepository;
 import com.andcris.cursomc.repositories.PedidoRepository;
 import com.andcris.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -13,11 +20,47 @@ import com.andcris.cursomc.services.exceptions.ObjectNotFoundException;
 public class PedidoService {
 
 	@Autowired
-	PedidoRepository repo;
+	private PedidoRepository repo;
+	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		
+		for(ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 }
