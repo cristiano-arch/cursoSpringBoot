9711a2e5647b435be613e8b900482ff5d3eebc37
Cristiano Jr <cristianoandre96@gmail.com>
2021-06-30 20:19:03 -0300

    Implementando autorizacao

--- src/main/java/com/andcris/cursomc/config/SecurityConfig.java
+++ src/main/java/com/andcris/cursomc/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.andcris.cursomc.security.JWTAuthenticationFilter;
+import com.andcris.cursomc.security.JWTAuthorizationFilter;
 import com.andcris.cursomc.security.JWTUtil;
 
 @Configuration
@@ -58,6 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
